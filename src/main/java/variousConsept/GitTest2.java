27ed9fd292048cc30ff22e23cdd9f668ac9a3249
thazvia <92962601+thazvia@users.noreply.github.com>
2021-11-18 18:57:22 -0600

    commit 3

---
+++ src/main/java/variousConsept/GitTest2.java
@@ -0,0 +1,12 @@
+package variousConsept;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+		System.out.println("git test");
+		System.out.println("git test1");
+		System.out.println("git test2");
+		System.out.println("git test3");
+	}
+
+}
